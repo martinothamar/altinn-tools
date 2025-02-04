bd342a187fa76a56e7bf45fdc81c86b7cf2a24a9
Martin Othamar <martin@othamar.net>
2025-02-04 22:24:52 +0100

    more tweaks

--- FindEventDeliveryErrors/Program.cs
+++ FindEventDeliveryErrors/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System.Globalization;
+using System.Net.Http.Json;
 using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Channels;
 using Azure;
 using Azure.Identity;
 using Azure.Monitor.Query;
@@ -7,9 +10,18 @@ using Azure.Monitor.Query.Models;
 using Azure.ResourceManager;
 using Azure.ResourceManager.OperationalInsights;
 using CsvHelper;
+using CsvHelper.Configuration.Attributes;
 using Spectre.Console;
+using SQLite;
 
-Directory.CreateDirectory("data");
+if (!Directory.Exists("data"))
+    Directory.CreateDirectory("data");
+if (!Directory.Exists("data/export"))
+    Directory.CreateDirectory("data/export");
+
+var db = new SQLiteAsyncConnection(Path.Combine("data", "data.db"));
+
+await db.CreateTableAsync<ErrorRecord>();
 
 var client = new ArmClient(new AzureCliCredential());
 
@@ -17,10 +29,33 @@ var cancellationToken = CancellationToken.None;
 
 var subscriptions = await client.GetSubscriptions().GetAllAsync().ToArrayAsync();
 
-int DaysToSearch = 30;
+var env = await File.ReadAllLinesAsync(".env");
+foreach (var line in env)
+{
+    var parts = line.Split('=', 2);
+    if (parts.Length != 2)
+        throw new Exception($"Invalid .env line: {line}");
+
+    var key = parts[0];
+    var value = parts[1];
+    if (Environment.GetEnvironmentVariable(key) is null)
+        Environment.SetEnvironmentVariable(key, value);
+}
+
+var slackAccessToken = Environment.GetEnvironmentVariable("SlackAccessToken");
+var slackSigningSecret = Environment.GetEnvironmentVariable("SlackSigningSecret");
+if (string.IsNullOrWhiteSpace(slackAccessToken) || string.IsNullOrWhiteSpace(slackSigningSecret))
+    throw new Exception("Missing SlackAccessToken or SlackSigningSecret. Check the '.env' file");
+
+const int SearchFromDays = 90;
 TimeSpan PollInterval = TimeSpan.FromHours(1);
 
-await Task.WhenAll(subscriptions.Select(async subscription => 
+var instanceIdInUrlRegex = new Regex(@"(\d+)\/([0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12})", RegexOptions.Compiled);
+
+long totalErrors = 0;
+long dbSavedErrors = 0;
+
+var tasks = subscriptions.Select(async subscription => 
 {
     // if (!subscription.Data.DisplayName.Contains("skd", StringComparison.OrdinalIgnoreCase))
     //     return;
@@ -38,47 +73,61 @@ await Task.WhenAll(subscriptions.Select(async subscription =>
     if (serviceOwner.Any(char.IsLower))
         return;
     
-    var serviceOwnerLower = serviceOwner.ToLowerInvariant();
+    serviceOwner = serviceOwner.ToLowerInvariant();
     var rgs = await subscription.GetResourceGroups().GetAllAsync(cancellationToken: cancellationToken).ToArrayAsync();
-    var rg = rgs.SingleOrDefault(rg => rg.Data.Name == $"monitor-{serviceOwnerLower}-prod-rg");
+    var rg = rgs.SingleOrDefault(rg => rg.Data.Name == $"monitor-{serviceOwner}-prod-rg");
     if (rg is null)
         return;
 
     var workspaces = await rg.GetOperationalInsightsWorkspaces().GetAllAsync(cancellationToken: cancellationToken).ToArrayAsync();
-    var workspace = workspaces.SingleOrDefault(ws => ws.Data.Name == $"application-{serviceOwnerLower}-prod-law");
+    var workspace = workspaces.SingleOrDefault(ws => ws.Data.Name == $"application-{serviceOwner}-prod-law");
     if (workspace is null)
         return;
 
+    var lastError = await db.Table<ErrorRecord>()
+        .Where(e => e.ServiceOwner == serviceOwner)
+        .OrderByDescending(e => e.TimeGenerated)
+        .FirstOrDefaultAsync();
+    var searchFrom = lastError is not null ? 
+                lastError.TimeGenerated : 
+                DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(SearchFromDays)).AddSeconds(-1);
+    AnsiConsole.MarkupLine( $"([bold]{serviceOwner}[/]) - initializing search from [blue]{searchFrom.UtcDateTime:o}[/]");
+
     var client = new LogsQueryClient(new AzureCliCredential());
-    var workspaceId = workspace.Id;
-
-    var query = $"""
-        AppDependencies
-        | where TimeGenerated > now(-{DaysToSearch}d)
-        | where Success == false
-        | where Target == "platform.altinn.no"
-        | where (Name startswith "POST /storage" and Name endswith "/events" and OperationName startswith "PUT Process/NextElement");
-
-        AppDependencies
-        | where TimeGenerated > now(-{DaysToSearch}d)
-        | where Success == false
-        | where Target == "platform.altinn.no"
-        | where (Name startswith "POST /events" and OperationName startswith "PUT Process/NextElement");
-    """;
 
     var timer = new PeriodicTimer(PollInterval);
     do 
     {
+        searchFrom = lastError is not null ? 
+            lastError.TimeGenerated : 
+            DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(SearchFromDays)).AddSeconds(-1);
+        var query = $"""
+            AppDependencies
+            | where TimeGenerated > todatetime('{searchFrom.UtcDateTime:o}')
+            | where Success == false
+            | where Target == "platform.altinn.no"
+            | where (Name startswith "POST /storage" and Name endswith "/events" and OperationName startswith "PUT Process/NextElement");
+
+            AppDependencies
+            | where TimeGenerated > todatetime('{searchFrom.UtcDateTime:o}')
+            | where Success == false
+            | where Target == "platform.altinn.no"
+            | where (Name startswith "POST /events" and OperationName startswith "PUT Process/NextElement")
+            | join kind=inner AppRequests on OperationId;
+        """;
         Response<LogsQueryResult> results = await client.QueryResourceAsync(
-            workspaceId,
+            workspace.Id,
             query,
-            new QueryTimeRange(TimeSpan.FromDays(DaysToSearch)),
+            new QueryTimeRange(TimeSpan.FromDays(SearchFromDays)),
             cancellationToken: cancellationToken
         );
 
         var tables = results.Value.AllTables;
+        if (tables.Sum(t => t.Rows.Count) == 0)
+            continue;
+
         var total = string.Join(", ", tables.Select((t, i) => $"error {i + 1}={(t.Rows.Count > 0 ? $"[red]{t.Rows.Count}[/]" : $"[green]{t.Rows.Count}[/]")}"));
-        AnsiConsole.MarkupLine( $"([bold]{serviceOwnerLower}[/]) - {total}");
+        AnsiConsole.MarkupLine( $"([bold]{serviceOwner}[/]) - {total}");
         
         int tableCount = 0;
         for (int i = 0; i < tables.Count; i++)
@@ -88,66 +137,282 @@ await Task.WhenAll(subscriptions.Select(async subscription =>
                 continue;
 
             tableCount++;
-            var records = table.Rows.Select(r => 
-            {
-                return new DependencyRecord(
-                    r.GetDateTimeOffset(nameof(DependencyRecord.TimeGenerated)) ?? default,
-                    r.GetString(nameof(DependencyRecord.Id)),
-                    r.GetString(nameof(DependencyRecord.Target)),
-                    r.GetString(nameof(DependencyRecord.DependencyType)),
-                    r.GetString(nameof(DependencyRecord.Name)),
-                    r.GetString(nameof(DependencyRecord.Data)),
-                    r.GetBoolean(nameof(DependencyRecord.Success)),
-                    r.GetString(nameof(DependencyRecord.ResultCode)),
-                    r.GetDouble(nameof(DependencyRecord.DurationMs)),
-                    r.GetString(nameof(DependencyRecord.PerformanceBucket)),
-                    r.GetString(nameof(DependencyRecord.Properties)),
-                    r.GetString(nameof(DependencyRecord.OperationName)),
-                    r.GetString(nameof(DependencyRecord.OperationId)),
-                    r.GetString(nameof(DependencyRecord.ParentId)),
-                    r.GetString(nameof(DependencyRecord.AppVersion)),
-                    r.GetString(nameof(DependencyRecord.AppRoleName))
-                );
-            }).ToArray();
 
+            var records = new ErrorRecord[table.Rows.Count];
+            for (int j = 0; j < table.Rows.Count; j++)
             {
-                var filename = $"data/{serviceOwnerLower}-error-{i + 1}.json";
-                File.Delete(filename);
-                await using var writer = File.OpenWrite(filename);
-                await JsonSerializer.SerializeAsync(writer, records, new JsonSerializerOptions { WriteIndented = true }, cancellationToken);
+                var r = table.Rows[j];
+                var name = r.GetString(nameof(ErrorRecord.Name));
+
+                string? rootRequestUrl = null;
+                int? instanceOwnerPartyId = null;
+                Guid? instanceId = null;
+                if (table.Columns.Any(c => c.Name == "Url"))
+                {
+                    // Url column is present in requests table but not the dependency table
+                    // so in this case the root trace/request has been joined in
+                    rootRequestUrl = r.GetString("Url");
+                    var match = instanceIdInUrlRegex.Match(rootRequestUrl);
+                    instanceOwnerPartyId = int.Parse(match.Groups[1].Value);
+                    instanceId = Guid.Parse(match.Groups[2].Value);
+                }
+                else 
+                {
+                    // When we don't have the root request joined in,
+                    // lets see if we can extract the instance id from the name
+                    var match = instanceIdInUrlRegex.Match(name);
+                    instanceOwnerPartyId = int.Parse(match.Groups[1].Value);
+                    instanceId = Guid.Parse(match.Groups[2].Value);
+                }
+
+                var timeGenerated = r.GetDateTimeOffset(nameof(ErrorRecord.TimeGenerated)) ?? default;
+
+                var record = new ErrorRecord
+                {
+                    ServiceOwner = serviceOwner,
+                    TimeGenerated = timeGenerated,
+                    InstanceOwnerPartyId = instanceOwnerPartyId,
+                    InstanceId = instanceId,
+                    Id = r.GetString(nameof(ErrorRecord.Id)),
+                    Target = r.GetString(nameof(ErrorRecord.Target)),
+                    DependencyType = r.GetString(nameof(ErrorRecord.DependencyType)),
+                    Name = name,
+                    Data = r.GetString(nameof(ErrorRecord.Data)),
+                    Success = r.GetBoolean(nameof(ErrorRecord.Success)),
+                    ResultCode = r.GetString(nameof(ErrorRecord.ResultCode)),
+                    DurationMs = r.GetDouble(nameof(ErrorRecord.DurationMs)),
+                    PerformanceBucket = r.GetString(nameof(ErrorRecord.PerformanceBucket)),
+                    Properties = r.GetString(nameof(ErrorRecord.Properties)),
+                    OperationName = r.GetString(nameof(ErrorRecord.OperationName)),
+                    OperationId = r.GetString(nameof(ErrorRecord.OperationId)),
+                    ParentId = r.GetString(nameof(ErrorRecord.ParentId)),
+                    AppVersion = r.GetString(nameof(ErrorRecord.AppVersion)),
+                    AppRoleName = r.GetString(nameof(ErrorRecord.AppRoleName)),
+                    AlertedInSlack = false,
+                    ErrorNumber = i + 1,
+                };
+
+                if (timeGenerated > searchFrom)
+                    lastError = record;
+
+                records[j] = record;
             }
 
-            {
-                var filename = $"data/{serviceOwnerLower}-error-{i + 1}.csv";
-                File.Delete(filename);
-                await using var writer = new StreamWriter(filename);
-                await using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
-                await csv.WriteRecordsAsync(records, cancellationToken);
-            }
+            Interlocked.Add(ref totalErrors, records.Length);
+
+            var savedCount = await db.InsertAllAsync(records);
+
+            Interlocked.Add(ref dbSavedErrors, savedCount);
+
+            // {
+            //     var filename = $"data/export/{serviceOwner}-error-{i + 1}.json";
+            //     await using var writer = File.OpenWrite(filename);
+            //     await JsonSerializer.SerializeAsync(writer, records, new JsonSerializerOptions { WriteIndented = true }, cancellationToken);
+            // }
+
+            // {
+            //     var filename = $"data/export/{serviceOwner}-error-{i + 1}.csv";
+            //     await using var writer = new StreamWriter(filename);
+            //     await using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+            //     await csv.WriteRecordsAsync(records, cancellationToken);
+            // }
         }
         
-        AnsiConsole.MarkupLine($"([bold]{serviceOwnerLower}[/]) - wrote [blue]{tableCount}[/] to 'data/', sleeping for [blue]{PollInterval}[/]...");
+        AnsiConsole.MarkupLine($"([bold]{serviceOwner}[/]) - wrote [blue]{tableCount}[/] to 'data/', sleeping for [blue]{PollInterval}[/]...");
     }
     while (await timer.WaitForNextTickAsync(cancellationToken));
+}).ToList();
+
+// tasks.Add(Task.Run(async () => 
+// {
+//     var timer = new PeriodicTimer(TimeSpan.FromSeconds(10));
+
+//     var httpClient = new HttpClient();
+//     httpClient.DefaultRequestHeaders.Authorization = new("Bearer", slackAccessToken);
+//     do 
+//     {
+//         var errors = await db.Table<ErrorRecord>()
+//             .Where(e => e.AlertedInSlack == false)
+//             .OrderBy(e => e.TimeGenerated)
+//             .ToArrayAsync();
+
+//         foreach (var error in errors)
+//         {
+//             var text = $"""
+//                 ALERT:
+//                 ```
+//                 app: {error.ServiceOwner}/{error.AppRoleName}/{error.AppVersion}
+//                 feil: {error.Name} (status {error.ResultCode}, {error.DurationMs}ms) kl: {error.TimeGenerated:O}
+//                 instanceOwnerPartyId: {error.InstanceOwnerPartyId}, instanceId: {error.InstanceId}
+//                 operation ID: {error.OperationId}
+//                 ```
+//             """.Trim();
+//             // var url = "https://slack.com/api/chat.postMessage";
+//             // using var response = await httpClient.PostAsJsonAsync(url, new 
+//             // {
+//             //     channel = ,
+//             //     text = text,
+//             // });
+//             // if (!response.IsSuccessStatusCode)
+//             // {
+//             //     var body = await response.Content.ReadAsStringAsync();
+//             //     throw new Exception($"Failed to send message to slack: {response.StatusCode}: {body}");
+//             // }
+
+//             await using (var writer = File.AppendText("data/export/slack.txt"))
+//             {
+//                 await writer.WriteLineAsync($"{text}\n");
+//             }
+
+//             error.AlertedInSlack = true;
+//             await db.UpdateAsync(error);
+//         }
+
+//     }
+//     while (await timer.WaitForNextTickAsync());
+// }));
+
+
+tasks.Add(Task.Run(async () => 
+{
+    var timer = new PeriodicTimer(TimeSpan.FromSeconds(30));
+    do 
+    {
+        var filename = $"data/export/all-errors.csv";
+        var fileInfo = new FileInfo(filename);
+
+        var lastRecord = await db.Table<ErrorRecord>()
+            .OrderByDescending(e => e.TimeGenerated)
+            .FirstOrDefaultAsync();
+
+        if (!fileInfo.Exists || lastRecord.TimeGenerated > fileInfo.LastWriteTime)
+        {
+            if (fileInfo.Exists)
+                fileInfo.Delete();
+
+            await using var writer = new StreamWriter(filename);
+            await using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+            var dbRecords = await db.Table<ErrorRecord>()
+                .OrderBy(r => r.TimeGenerated)
+                .ToArrayAsync();
+
+            var records = dbRecords.Select(r => new ErrorRecordForCsv
+            {
+                TimeGenerated = r.TimeGenerated,
+                ServiceOwner = r.ServiceOwner,
+                AppRoleName = r.AppRoleName,
+                AppVersion = r.AppVersion,
+                InstanceOwnerPartyId = r.InstanceOwnerPartyId,
+                InstanceId = r.InstanceId,
+
+                Id = r.Id,
+                OperationId = r.OperationId,
+                ParentId = r.ParentId,
+                OperationName = r.OperationName,
+                Name = r.Name,
+                Success = r.Success,
+                ResultCode = r.ResultCode,
+                DurationMs = r.DurationMs,
+                PerformanceBucket = r.PerformanceBucket,
+                Target = r.Target,
+                DependencyType = r.DependencyType,
+                Data = r.Data,
+                Properties = r.Properties,
+                AlertedInSlack = r.AlertedInSlack,
+                ErrorNumber = r.ErrorNumber,
+            }).ToArray();
+            await csv.WriteRecordsAsync(records);
+        }
+    }
+    while (await timer.WaitForNextTickAsync());
 }));
 
+tasks.Add(Task.Run(async () => 
+{
+    var timer = new PeriodicTimer(TimeSpan.FromSeconds(10));
+    do 
+    {
+        AnsiConsole.MarkupLine($"[bold]Total errors:[/] {totalErrors}, [bold]DB saved:[/] {dbSavedErrors}");
+    }
+    while (await timer.WaitForNextTickAsync());
+}));
 
-public sealed record DependencyRecord(
-    DateTimeOffset TimeGenerated, 
-    string Id, 
-    string Target, 
-    string DependencyType, 
-    string Name, 
-    string Data, 
-    bool? Success, 
-    string ResultCode,
-    double? DurationMs,
-    string PerformanceBucket,
-    string Properties,
-    string OperationName,
-    string OperationId,
-    string ParentId,
-    string AppVersion,
-    string AppRoleName
-);
+await Task.WhenAll(tasks);
+
+
+public sealed class ErrorRecord
+{
+	[PrimaryKey, AutoIncrement]
+	public int PK { get; set; }
+
+	[Indexed]
+    public string? ServiceOwner { get; init; }
+    
+	[Indexed]
+    public DateTimeOffset TimeGenerated { get; init; }
+
+	[Indexed]
+    public int? InstanceOwnerPartyId { get; init; }
+	[Indexed]
+    public Guid? InstanceId { get; init; }
+	[Indexed]
+    public string? Id { get; init; } 
+    public string? Target { get; init; } 
+    public string? DependencyType { get; init; } 
+    public string? Name { get; init; } 
+    public string? Data { get; init; } 
+	[Indexed]
+    public bool? Success { get; init; } 
+	[Indexed]
+    public string? ResultCode { get; init; }
+    public double? DurationMs { get; init; }
+    public string? PerformanceBucket { get; init; }
+    public string? Properties { get; init; }
+    public string? OperationName { get; init; }
+	[Indexed]
+    public string? OperationId { get; init; }
+    public string? ParentId { get; init; }
+	[Indexed]
+    public string? AppVersion { get; init; }
+	[Indexed]
+    public string? AppRoleName { get; init; }
+
+    [Indexed]
+    public bool AlertedInSlack { get; set; }
+
+    [Indexed]
+    public int ErrorNumber { get; set; }
+}
+
+
+public sealed class ErrorRecordForCsv
+{
+    [Format("dd.MM.yyyy HH:mm:ss")]
+    public DateTimeOffset TimeGenerated { get; init; }
+
+    public string? ServiceOwner { get; init; }
+    public string? AppRoleName { get; init; }
+    public string? AppVersion { get; init; }
+    public int? InstanceOwnerPartyId { get; init; }
+    public Guid? InstanceId { get; init; }
+    
+    public string? Id { get; init; } 
+    public string? OperationId { get; init; }
+    public string? ParentId { get; init; }
+    public string? OperationName { get; init; }
+    public string? Name { get; init; } 
+    public bool? Success { get; init; } 
+    public string? ResultCode { get; init; }
+    public double? DurationMs { get; init; }
+    public string? PerformanceBucket { get; init; }
+    public string? Target { get; init; } 
+    public string? DependencyType { get; init; } 
+    public string? Data { get; init; }     
+    public string? Properties { get; init; }
+    public bool AlertedInSlack { get; set; }
+    public int ErrorNumber { get; set; }
+}
+
+
 
